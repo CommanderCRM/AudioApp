86406fc4e1254d3159496a5a418d45db34bc4e6a
CommanderCRM <htctomsk634@gmail.com>
2023-09-07 11:16:20 +0700

    get file name tool, dynamic file reference

--- audiotools/tools.py
+++ audiotools/tools.py
@@ -1,3 +1,4 @@
+import os
 import librosa
 import soundfile as sf
 
@@ -8,10 +9,14 @@ class AudioFile:
         self.file = file
 
     def load(self):
-        """Загрузка информации о файле (аудиоряд, частота дискретизации по умолчанию)"""
+        """Загрузка файла (аудиоряд, частота дискретизации по умолчанию)"""
         y, sr = librosa.load(self.file, sr=None)
         return y, sr
 
     def save(self, y, sr, outfile):
         """Запись аудиофайла в новый"""
         sf.write(outfile, y, sr)
+
+    def get_file_name(self):
+        """Получение имени аудиофайла"""
+        return os.path.basename(self.file)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from audiosegm.segmentation import Segmentation
 
 file = AudioFile('1.wav')
 y, sr = file.load()
+file_name = file.get_file_name()
 file.save(y, sr, '2.wav')
 
 audio_preprocessor = AudioPreprocessor()
@@ -12,15 +13,17 @@ reduced_audio = audio_preprocessor.reduce_noise(y, sr)
 file.save(reduced_audio, sr, "3.wav")
 
 file5 = AudioFile('5.wav')
+file5_name = file5.get_file_name()
 y5, sr5 = file5.load()
 
 metrics = AudioMetrics()
-print('DTW:', metrics.get_audio_dtw(y, y5))
-print('ERP:', metrics.get_audio_erp(y, y5))
-print('EDR:', metrics.get_audio_edr(y, y5))
+print(f'DTW between {file_name} and {file5_name}:', metrics.get_audio_dtw(y, y5))
+print(f'ERP between {file_name} and {file5_name}:', metrics.get_audio_erp(y, y5))
+print(f'EDR between {file_name} and {file5_name}:', metrics.get_audio_edr(y, y5))
 
 file8 = AudioFile('8.wav')
+file8_name = file8.get_file_name()
 y8, sr8 = file8.load()
 
 segm = Segmentation()
-print('Segments:', segm.voiced_unvoiced_silence(y8, sr8))
+print(f'Segments of {file8_name}:', segm.voiced_unvoiced_silence(y8, sr8))
