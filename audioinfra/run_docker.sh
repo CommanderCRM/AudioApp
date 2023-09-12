282f8a6efdb47b03e5d886c85c1eb5b3bfb33f30
CommanderCRM <htctomsk634@gmail.com>
2023-09-12 10:59:56 +0700

    infra folder

--- audioinfra/run_docker.sh
+++ audioinfra/run_docker.sh
@@ -4,6 +4,7 @@ DOCKER_NAME="audiodocker"
 HOST_PORT=48080
 CONTAINER_PORT=48080
 SCRIPT_NAME="run_server.sh"
+FOLDER_NAME="audioinfra"
 
 # Запуск в интерактивном режиме с удалением контейнера после остановки
 # Установка портов, монтирование директории, запуск скрипта
@@ -11,6 +12,6 @@ docker run\
             -it\
             --rm\
             -p $HOST_PORT:$CONTAINER_PORT\
-            -v "$(pwd)":/app\
+            -v "$(pwd)/..":/app\
             $DOCKER_NAME\
-            -c "bash ./$SCRIPT_NAME"
+            -c "bash ./$FOLDER_NAME/$SCRIPT_NAME"
--- audioinfra/run_server.sh
+++ audioinfra/run_server.sh
@@ -4,6 +4,8 @@ UVICORN_PORT=48080
 APP_NAME="server"
 FOLDER_NAME="audioserver"
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # Переходим в папку с сервером и запускаем его локально
-cd ./$FOLDER_NAME || exit
+cd "$SCRIPT_DIR"/.. && cd $FOLDER_NAME || exit
 uvicorn $APP_NAME:app --host 0.0.0.0 --port $UVICORN_PORT
