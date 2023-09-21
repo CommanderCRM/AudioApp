9c9fb8605c0b78c78ccce4b95b7390cf3071664c
Borovskoy Aleksey <gladiatormahotina@yandex.ru>
2023-09-21 22:42:37 +0700

    Правки в API и БД
    API:
    session_score:
    type:integer -> type:number
    example: 50 -> example 50.5
    base64_segment_value - nullable: true
    Поправил все баги из kaiten
    
    БД:
    добавил таблицу для связи пациента и врачей. изменил атрибут town на hospital
    объединил таблицы речи и эталона
    добавил новые атрибуты в таблицу сеансов (в соответствии с ПО)
    добавил таблицу слогов и фраз

--- audioinfra/sqldocker/init.sql
+++ audioinfra/sqldocker/init.sql
@@ -1,51 +1,43 @@
+-- DDL Generated from https:/databasediagram.com
+
 CREATE TABLE patient_table (
-  medical_card_number VARCHAR(12) PRIMARY KEY,
+  card_number VARCHAR(12) PRIMARY KEY,
   full_name VARCHAR(255),
   gender CHAR(1),
   constant_password VARCHAR(128),
+  hospital VARCHAR(64),
   temporary_password VARCHAR(128),
   is_password_changed BOOL,
   date_of_birth DATE,
   town VARCHAR(64)
 );
 
-CREATE TABLE reference_table (
-  reference_id serial PRIMARY KEY,
-  reference_name VARCHAR(50),
-  referenct_type VARCHAR(50),
-  medical_card_number VARCHAR(12) REFERENCES patient_table(medical_card_number),
-  base64_value VARCHAR
-);
-
 CREATE TABLE speech_table (
   speech_id serial PRIMARY KEY,
   speech_type VARCHAR(50),
-  medical_card_number VARCHAR(12) REFERENCES patient_table(medical_card_number),
-  base64_value VARCHAR
-);
-
-CREATE TABLE speech_quality_table (
-  speech_quality_id serial PRIMARY KEY,
-  score int,
-  reference_id int REFERENCES reference_table(reference_id),
-  speech_id int REFERENCES speech_table(speech_id)
+  base64_value VARCHAR,
+  base64_segmet_value VARCHAR,
+  speech_score REAL,
+  is_reference_speech BOOL
 );
 
 CREATE TABLE session_table (
   session_id serial PRIMARY KEY,
-  medical_card_number VARCHAR(12) REFERENCES patient_table(medical_card_number)
+  is_reference_session BOOL,
+  session_score REAL,
+  card_number VARCHAR(12) REFERENCES patient_table(card_number)
 );
 
-CREATE TABLE speech_quality_session_table (
-  speech_quality_session_id serial PRIMARY KEY,
-  speech_quality_id int REFERENCES speech_quality_table(speech_quality_id),
+CREATE TABLE speech_session_table (
+  speech_session_id serial PRIMARY KEY,
+  speech_id int REFERENCES speech_table(speech_id),
   session_id int REFERENCES session_table(session_id)
 );
 
 CREATE TABLE doctor_table (
   username VARCHAR(255) PRIMARY KEY,
   specialization VARCHAR(255),
-  town VARCHAR(64),
+  hospital VARCHAR(64),
   password VARCHAR(128),
   full_name VARCHAR(255)
 );
@@ -53,4 +45,19 @@ CREATE TABLE doctor_table (
 CREATE TABLE specialist_table (
   username VARCHAR(255) PRIMARY KEY,
   password VARCHAR(128)
+);
+
+CREATE TABLE doctor_patient_table (
+  doctor_patient_id serial PRIMARY KEY,
+  doctor_username VARCHAR(255) REFERENCES doctor_table(username),
+  patient_card_number VARCHAR(12) REFERENCES patient_table(card_number)
+);
+
+CREATE TABLE syllables_phrases_table (
+  syllable_phrase_id serial PRIMARY KEY,
+  type VARCHAR(10),
+  dict VARCHAR(10),
+  value VARCHAR(50),
+  start_position int,
+  end_position int
 );
