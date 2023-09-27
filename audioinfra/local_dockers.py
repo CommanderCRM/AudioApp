c8889d00bcb2aa10d2aa7d61534ffa96f7de1536
CommanderCRM <htctomsk634@gmail.com>
2023-09-27 10:55:26 +0700

    nosec

--- audioinfra/local_dockers.py
+++ audioinfra/local_dockers.py
@@ -1,18 +1,18 @@
 import argparse
-import subprocess
+import subprocess # nosec
 import os
 
 def save_images():
     """Получение списка образов Docker в системе и сохранение их в .tar архивы"""
     command = ['docker', 'images', '--format', '{{.Repository}}:{{.Tag}}']
-    result = subprocess.run(command, capture_output=True, text=True, check=True)
+    result = subprocess.run(command, capture_output=True, text=True, check=True) # nosec
     images = result.stdout.split('\n')
 
     for image in images:
         if image:
             file_name = image.replace('/', '_').rstrip(':-')
             command = ['docker', 'save', '-o', f'{file_name}.tar', image]
-            subprocess.run(command, check=True)
+            subprocess.run(command, check=True) # nosec
 
 def install_images():
     """Установка образов Docker из архивов .tar"""
@@ -22,7 +22,7 @@ def install_images():
 
     for file in tar_files:
         command = ['docker', 'load', '-i', file]
-        subprocess.run(command, check=True)
+        subprocess.run(command, check=True) # nosec
 
 HELP_MESSAGE='Данный скрипт позволяет сохранить Docker-образы в архивы .tar и установить их из .tar'
 parser = argparse.ArgumentParser(description=f'{HELP_MESSAGE}')
