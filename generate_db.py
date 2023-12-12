a0471490dcdf8120ecafca8f660706a9060bdd17
CommanderCRM <htctomsk634@gmail.com>
2023-12-12 11:02:47 +0700

    script fixes, db additions

--- generate_db.py
+++ generate_db.py
@@ -1,7 +1,9 @@
 import secrets
 import string
+from pathlib import Path
 from transliterate import translit
 from audioserver.logic.secactions import hash_gost_3411
+from audioserver.logic.audiotools.tools import AudioFile
 
 def generate_password(pass_length: int) -> str:
     """Генерация пароля"""
@@ -31,13 +33,60 @@ def generate_doctors():
     for idx, value in enumerate(names):
         if idx > 10:
             hospital = "Moscow NII_1"
+            specialization = "logoped_onkolog"
         else:
             hospital = "Tomsk NII"
+            specialization = "logoped"
 
-        print(f"INSERT INTO doctor_table VALUES ('user{idx}', 'logoped', '{hospital}', '{hash_gost_3411(generate_password(9))}', '{transliterate_ru_to_en(value)} {transliterate_ru_to_en(surnames[idx])}');")
+        print(f"INSERT INTO doctor_table VALUES ('user{idx}', '{specialization}', '{hospital}', '{hash_gost_3411(generate_password(9))}', '{transliterate_ru_to_en(value)} {transliterate_ru_to_en(surnames[idx])}');")
 
 def generate_specialists():
     """Генерация специалистов для БД"""
-    return
 
-generate_doctors()
+    for i in range(21, 40):
+        print(f"INSERT INTO specialist_table VALUES ('user{i}', '{hash_gost_3411(generate_password(9))}');")
+
+def generate_signal():
+    """Генерация звукового сигнала"""
+
+    phrases = ["Белая пелена лежала на полях",
+                "В школу приезжали герои фронта",
+                "Белый пар расстилается над лужами",
+                "Экипаж танка понял задачу",
+                "Этот блок работает хорошо",
+                "Начинаются степные пожары",
+                "Ученики поливают огород",
+                "Тяжелый подъем закончился",
+                "Тропинка уперлась в глинистый уступ",
+                "Солнце поднялось над лесом",
+                "В подъезде стояли санитары",
+                "Стало известно место встречи",
+                "На участке ведется наблюдение",
+                "В класс вошел преподаватель",
+                "Полено раскололось надвое",
+                "Надо зарядить ружье",
+                "Телега начала скрипеть",
+                "Мать отвела ребенка в сад",
+                "Ребята сидели на берегу",
+                "В магазине продаются яблоки",
+                "Директор сравнил доход с расходом",
+                "Высокая рожь колыхалась",
+                "Цветы пестрели в долине",
+                "Чудный запах леса освежает",
+                "День был удивительно хорош"]
+
+    cwd = Path.cwd()
+    syllables = cwd / 'slogs'
+
+    for idx, value in enumerate(phrases):
+        if idx % 3 != 0:
+            is_reference_flag = 1
+        else:
+            is_reference_flag = 0
+
+        path = syllables / f'{idx+1}.wav'
+        audio = AudioFile(path)
+
+        base64 = audio.encode_base64()
+        base64_truncated = base64[:150]
+        print(f"INSERT INTO signal_table (signal_type, base64_value, is_reference_signal, real_value) VALUES ('фраза', '{base64_truncated}', '{is_reference_flag}', '{value}');")
