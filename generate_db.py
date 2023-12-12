7eb78820e90939fd60890c7d58da39c74c1e7b35
CommanderCRM <htctomsk634@gmail.com>
2023-12-12 10:14:30 +0700

    minor fixes

--- generate_db.py
+++ generate_db.py
@@ -1,15 +1,26 @@
-import random
-from transliterate import translit, get_available_language_codes
-from audioserver.logic.secactions import hash_gost_3411
 import secrets
 import string
-alphabet = string.ascii_letters + string.digits
+from transliterate import translit
+from audioserver.logic.secactions import hash_gost_3411
 
-names = ['Евгений', 'Денис', 'Антон', 'Игорь', 'Юрий', 'Олег', 'Вячеслав', 'Василий', 'Станислав', 'Вадим', 'Елена', 'Наталья', 'Ольга', 'Юлия', 'Татьяна', 'Ирина', 'Светлана', 'Марина', 'Надежда', 'Любовь']
-surnames = ['Иванов', 'Смирнов', 'Кузнецов', 'Попов', 'Васильев', 'Петров', 'Соколов', 'Михайлов', 'Новиков', 'Фёдоров', 'Конева', 'Полянская', 'Снегова', 'Карпова', 'Матвеева', 'Медведева', 'Белая', 'Полякова', 'Фролова', 'Левченко']
+def generate_doctors():
+    """Генерация врачей для БД"""
+    alphabet = string.ascii_letters + string.digits
 
-for i in range(len(names)):
-    if i > 10:
-        print(f"INSERT INTO doctor_table VALUES ('user{i}', 'logoped', 'Moscow NII_1', '{hash_gost_3411(''.join(secrets.choice(alphabet) for i in range(9)))}', '{translit(names[i], language_code='ru', reversed=True)} {translit(surnames[i], language_code='ru', reversed=True)}');")
-    else:
-        print(f"INSERT INTO doctor_table VALUES ('user{i}', 'logoped onkolog', 'Tomsk NII', '{hash_gost_3411(''.join(secrets.choice(alphabet) for i in range(9)))}', '{translit(names[i], language_code='ru', reversed=True)} {translit(surnames[i], language_code='ru', reversed=True)}');")
+    names = ['Евгений', 'Денис', 'Антон', 'Игорь', 'Юрий', 'Олег', 'Вячеслав',
+            'Василий', 'Станислав', 'Вадим',
+            'Елена', 'Наталья', 'Ольга', 'Юлия', 'Татьяна', 'Ирина',
+            'Светлана', 'Марина', 'Надежда', 'Любовь']
+
+    surnames = ['Иванов', 'Смирнов', 'Кузнецов', 'Попов', 'Васильев', 'Петров',
+                'Соколов', 'Михайлов', 'Новиков',
+                'Фёдоров', 'Конева', 'Полянская', 'Снегова', 'Карпова', 'Матвеева',
+                'Медведева', 'Белая', 'Полякова', 'Фролова', 'Левченко']
+
+    for idx, value in enumerate(names):
+        if idx > 10:
+            print(f"INSERT INTO doctor_table VALUES ('user{idx}', 'logoped', 'Moscow NII_1', '{hash_gost_3411(''.join(secrets.choice(alphabet) for i in range(9)))}', '{translit(value, language_code='ru', reversed=True)} {translit(surnames[idx], language_code='ru', reversed=True)}');")
+        else:
+            print(f"INSERT INTO doctor_table VALUES ('user{idx}', 'logoped onkolog', 'Tomsk NII', '{hash_gost_3411(''.join(secrets.choice(alphabet) for i in range(9)))}', '{translit(value, language_code='ru', reversed=True)} {translit(surnames[idx], language_code='ru', reversed=True)}');")
+
+generate_doctors()
